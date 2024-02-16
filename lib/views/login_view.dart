4df32a2d604090aeb526d252eae9c19cb1b3ed28
Shivang Sorout <shivang.sorout@gmail.com>
2024-02-16 20:43:39 +0530

    step-4

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'dart:developer' as devtools show log;
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -56,15 +57,19 @@ class _LoginViewState extends State<LoginView> {
               final email = _email.text;
               final password = _password.text;
               try {
-                final userCredential =
-                    await FirebaseAuth.instance.signInWithEmailAndPassword(
-                  email: email,
-                  password: password,
-                );
-                print(userCredential);
+                await FirebaseAuth.instance
+                    .signInWithEmailAndPassword(
+                      email: email,
+                      password: password,
+                    )
+                    .then((value) =>
+                        Navigator.of(context).pushNamedAndRemoveUntil(
+                          '/notes/',
+                          (route) => false,
+                        ));
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'invalid-credential') {
-                  print('Invalid credentials entered!');
+                  devtools.log('Invalid credentials entered!');
                 }
               }
             },
--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'dart:developer' as devtools show log;
 
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
@@ -61,16 +62,16 @@ class _RegisterViewState extends State<RegisterView> {
                   email: email,
                   password: password,
                 );
-                print(userCredential);
+                devtools.log(userCredential.toString());
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'weak-password') {
-                  print('Weak password entered.');
+                  devtools.log('Weak password entered.');
                 } else if (e.code == 'email-already-in-use') {
-                  print('Email is already in use.');
+                  devtools.log('Email is already in use.');
                 } else if (e.code == 'invalid-email') {
-                  print('Email typed is invalid.');
+                  devtools.log('Email typed is invalid.');
                 } else {
-                  print(e.code);
+                  devtools.log(e.code);
                 }
               }
             },
