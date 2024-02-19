6fea2297b149394f4c5885381e2f6937c6bd4b0e
Shivang Sorout <shivang.sorout@gmail.com>
2024-02-20 05:27:28 +0530

    step-8

---
+++ lib/services/auth/auth_service.dart
@@ -0,0 +1,37 @@
+import 'package:mynotes/services/auth/auth_user.dart';
+import 'package:mynotes/services/auth/auth_provider.dart';
+
+class AuthService implements AuthProvider {
+  final AuthProvider provider;
+
+  AuthService(this.provider);
+
+  @override
+  Future<AuthUser> createUser({
+    required email,
+    required password,
+  }) =>
+      provider.createUser(
+        email: email,
+        password: password,
+      );
+
+  @override
+  AuthUser? get currentUser => provider.currentUser;
+
+  @override
+  Future<AuthUser> logIn({
+    required email,
+    required password,
+  }) =>
+      provider.logIn(
+        email: email,
+        password: password,
+      );
+
+  @override
+  Future<void> logOut() => provider.logOut();
+
+  @override
+  Future<void> sendEmailVerification() => provider.sendEmailVerification();
+}
