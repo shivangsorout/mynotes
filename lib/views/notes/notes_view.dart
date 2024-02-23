9fe22119ab79692bcb924a6664e49586808665eb
Shivang Sorout <shivang.sorout@gmail.com>
2024-02-24 02:55:55 +0530

    step-13

--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -34,6 +34,12 @@ class _NotesViewState extends State<NotesView> {
       appBar: AppBar(
         title: const Text('Main UI'),
         actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).pushNamed(newNoteRoute);
+            },
+            icon: const Icon(Icons.add),
+          ),
           PopupMenuButton<MenuAction>(
             onSelected: (value) async {
               switch (value) {
