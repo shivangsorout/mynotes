76927c565a7e6858eafe8c1b871b315ce25f8ec2
Shivang Sorout <shivang.sorout@gmail.com>
2024-02-27 19:18:49 +0530

    step-16

---
+++ lib/utilities/dialogs/logout_dialog.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:mynotes/utilities/dialogs/generic_dialog.dart';
+
+Future<bool> showLogOutDialog(
+  BuildContext context,
+) {
+  return showGenericDialog<bool>(
+    context: context,
+    title: 'Logout',
+    content: 'Are you sure you want to logout?',
+    optionBuilder: () => {
+      'Cancel': false,
+      'Log Out': true,
+    },
+  ).then((value) => value ?? false);
+}
