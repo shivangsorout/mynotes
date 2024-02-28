70a237d3d10589cc95c7de531f48e2971a9498e9
Shivang Sorout <shivang.sorout@gmail.com>
2024-02-28 19:09:38 +0530

    step-19

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -3,16 +3,19 @@ import 'package:flutter/foundation.dart';
 
 @immutable
 class AuthUser {
+  final String id;
   final bool isEmailVerified;
-  final String? email;
+  final String email;
 
   const AuthUser({
+    required this.id,
     required this.isEmailVerified,
     required this.email,
   });
 
   factory AuthUser.fromFirebase(User user) => AuthUser(
         isEmailVerified: user.emailVerified,
-        email: user.email,
+        email: user.email!,
+        id: user.uid,
       );
 }
--- test/auth_test.dart
+++ test/auth_test.dart
@@ -104,7 +104,11 @@ class MockAuthProvider implements AuthProvider {
     if (!isInitialized) throw NotInitializedException();
     if (email == 'foobar@mail.com') throw UserNotFoundAuthException();
     if (password == 'foo@bar.com') throw InvalidCredentialAuthException();
-    const user = AuthUser(isEmailVerified: false, email: 'demo@email.com');
+    const user = AuthUser(
+      isEmailVerified: false,
+      email: 'demo@email.com',
+      id: 'my_id',
+    );
     _user = user;
     return Future.value(user);
   }
@@ -122,7 +126,11 @@ class MockAuthProvider implements AuthProvider {
     if (!isInitialized) throw NotInitializedException();
     final user = _user;
     if (user == null) throw UserNotFoundAuthException();
-    const newUser = AuthUser(isEmailVerified: true, email: 'demo@email.com');
+    const newUser = AuthUser(
+      isEmailVerified: true,
+      email: 'demo@email.com',
+      id: 'my_id',
+    );
     _user = newUser;
   }
 }
